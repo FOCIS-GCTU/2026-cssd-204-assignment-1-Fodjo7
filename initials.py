38dc37de39e560a5ebe57f26934ee200d452c620
Fodjo7 <2425400448@live.gctu.edu.gh>
2026-05-30 15:26:03 +0000

    Submit assignment

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 2
+#
+# Name: Fodjo Matthew
+# STUDENT ID: 2425400448
+# Email: 2425400448@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Fodjo Matthew, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
 
 def main():
-  pass #code goes here
+  #Print out my initials
 
+  print("print out my initials\n \n")
 
+  print("...FMK\n \n \n\n")
+
+  print("...FFFFFFFFFFF........MMMM....MMMM........KKKK......KKK....")
+  print("...FFFFFFFFFFF........MMMM....MMMM........KKKK.....KKK.....")
+  print("...FFF................MM.MM..MM.MM........KKKK....KKK......")
+  print("...FFF................MM.MM..MM.MM........KKKK...KKK.......")
+  print("...FFFFFFFFFFF........MM.MM..MM.MM........KKKKKKK..........")
+  print("...FFFFFFFFFFF........MM..MMMM..MM........KKKKKKK..........")
+  print("...FFF................MM..MMMM..MM........KKKK...KKK.......")
+  print("...FFF................MM...MM...MM........KKKK....KKK......")
+  print("...FFF...........**...MM...MM...MM...**...KKKK.....KKK....**")
+  print("...FFF...........**...MM...MM...MM...**...KKKK......KKK....**\n\n\n")
 
 main()
